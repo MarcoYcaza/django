df4346c932754828f9c6790ad405565f61aa9e88
Marco Alexis Ycaza Lengua <ycaza.marco@gmail.com>
2021-04-10 21:00:11 -0500

    Delete neuronarebelde10042021/appix directory

--- neuronarebelde10042021/appix/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppixConfig(AppConfig):
-    name = 'appix'
--- neuronarebelde10042021/appix/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-
-class MyAnswerForm(forms.Form):
-    answer = forms.CharField(
-        widget=forms.Textarea(attrs={"style": "height: 100px;width:100%"})
-    )
